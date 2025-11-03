5ed052ab9418497ece2ddf03accc34de2087165b
User1 <User1@MSI>
2025-11-03 18:48:20 +0200

    Program.cs is simplified and cleared. Added ServiceCollectionExtensions

--- LiteratureSolitaire/Program.cs
+++ LiteratureSolitaire/Program.cs
@@ -11,14 +11,10 @@ namespace LiteratureSolitaire
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddApplicationDbContext(builder.Configuration);
+            builder.Services.AddApplicationIdentity(builder.Configuration);
             builder.Services.AddControllersWithViews();
+            builder.Services.AddApplicationServices(builder.Configuration);
 
             var app = builder.Build();
 

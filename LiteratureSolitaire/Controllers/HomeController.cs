e761eade1097e512522b35abd87f161f20337a4f
User1 <User1@MSI>
2026-01-19 22:09:57 +0200

    Added Game Rules

--- LiteratureSolitaire/Controllers/HomeController.cs
+++ LiteratureSolitaire/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace LiteratureSolitaire.Controllers
             return View();
         }
 
+        public IActionResult Rules()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
